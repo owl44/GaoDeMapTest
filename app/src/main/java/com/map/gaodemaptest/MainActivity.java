c3234ee4608ced6ecd2752db0424eaa7eda602fd
zrj <1106977428@qq.com>
2018-08-29 23:58:10 +0800

    a message

--- app/src/main/java/com/map/gaodemaptest/MainActivity.java
+++ app/src/main/java/com/map/gaodemaptest/MainActivity.java
@@ -1,10 +1,22 @@
 package com.map.gaodemaptest;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.MapView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     MapView mMapView = null;
@@ -14,15 +26,97 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //获取地图控件引用
-        mMapView = (MapView) findViewById(R.id.map);
+        mMapView = findViewById(R.id.map);
+
+        String s = sHA1(this);
+        Log.e("response",s);
         //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
         mMapView.onCreate(savedInstanceState);
         //初始化地图控制器对象
         if (aMap == null) {
             aMap = mMapView.getMap();
-            //该方法在AMap类中提供
-            aMap.setMapCustomEnable(true);//true 开启; false 关闭
+
+            setMapCustomStyleFile(this);
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(2000);
+                        //该方法在AMap类中提供
+                        aMap.setMapCustomEnable(true);//true 开启; false 关闭
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
         }
+    }
+
+    public static String sHA1(Context context) {
+        try {
+            PackageInfo info = context.getPackageManager().getPackageInfo(
+                    context.getPackageName(), PackageManager.GET_SIGNATURES);
+            byte[] cert = info.signatures[0].toByteArray();
+            MessageDigest md = MessageDigest.getInstance("SHA1");
+            byte[] publicKey = md.digest(cert);
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < publicKey.length; i++) {
+                String appendString = Integer.toHexString(0xFF & publicKey[i])
+                        .toUpperCase(Locale.US);
+                if (appendString.length() == 1)
+                    hexString.append("0");
+                hexString.append(appendString);
+                hexString.append(":");
+            }
+            String result = hexString.toString();
+            return result.substring(0, result.length()-1);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private void setMapCustomStyleFile(Context context) {
+        String styleName = "style_json.json";
+        FileOutputStream outputStream = null;
+        InputStream inputStream = null;
+        String filePath = null;
+        try {
+            inputStream = context.getAssets().open(styleName);
+            byte[] b = new byte[inputStream.available()];
+            inputStream.read(b);
+
+            filePath = context.getFilesDir().getAbsolutePath();
+            File file = new File(filePath + "/" + styleName);
+            if (file.exists()) {
+                file.delete();
+            }
+            file.createNewFile();
+            outputStream = new FileOutputStream(file);
+            outputStream.write(b);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (inputStream != null)
+                    inputStream.close();
+
+                if (outputStream != null)
+                    outputStream.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        aMap.setCustomMapStylePath(filePath + "/" + styleName);
+
+        aMap.showMapText(false);
+
     }
 
     @Override
